402616bc4bfc5b8e82b8bf45fc1745ac6fcf1b13
Trevor Gardner <trevorthebike@gmail.com>
2024-03-01 23:27:00 -0800

    added prototype videos, and added download links

--- pages/download.tsx
+++ pages/download.tsx
@@ -10,7 +10,7 @@ export default function Download() {
     <div className={`min-h-screen flex flex-col ${inter.className}`}>
       {/* Navigation Bar */}
       <nav className="w-full flex justify-between items-center p-4 shadow-md text-4xl">
-      <Link href="/"><h1>Magic Anvil</h1></Link>
+        <Link href="/"><h1>Magic Anvil</h1></Link>
         <div className="flex gap-16 text-2xl">
           <Link href="/member">Team Members</Link>
           <Link href="/credit">Credits</Link>
@@ -18,8 +18,15 @@ export default function Download() {
           <Link href="/prototype">Prototypes</Link>
         </div>
       </nav>
-      <h1>Downloads</h1>
-      <p>This is the credits page.</p>
+
+      <div className="flex-1 flex flex-col items-center justify-center">
+        <h1 className="text-5xl mb-8">Downloads</h1>
+        <p className="text-xl mb-8">This is the downloads page.</p>
+        {/* GitHub Link Button */}
+        <a href="https://github.com/Dalouma/Magic-Anvil" target="_blank" rel="noopener noreferrer" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Visit our GitHub
+        </a>
+      </div>
     </div>
   );
 }
